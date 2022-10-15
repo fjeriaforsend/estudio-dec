5305359d418e529159206972e4193ed99751305c
Vladimir Torres Muñoz <vladimir@forsend.cl>
2022-10-15 17:48:59 -0300

    Update content-home-page.php

--- wp-content/themes/estudiodec/template-parts/content-home-page.php
+++ wp-content/themes/estudiodec/template-parts/content-home-page.php
@@ -9,46 +9,21 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+<article id="post-<?php the_ID(); ?>" <?php post_class('container-fluid'); ?>>
 
-	<?php modulos_post_thumbnail(); ?>
+<div id="noticias" class="container">
+<h2 class="title-noticia-home w-100 text-center"><?php the_field('titulo_noticias'); ?></h2>
 
-	<div class="entry-content">
-		<?php
-		the_content();
+<picture>
+<?php 
+$image = get_field('foto_quienes_somos');
+if( !empty( $image ) ): ?>
+    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+<?php endif; ?>
+</picture>
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'modulos' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'modulos' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+
+</div>
+
 </article><!-- #post-<?php the_ID(); ?> -->
